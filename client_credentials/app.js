cf3e4beb24276ca0243622dde300b02657671e4b
ert00034 <31933424+ert00034@users.noreply.github.com>
2018-04-23 17:01:43 -0500

    hi
    hi

--- client_credentials/app.js
+++ client_credentials/app.js
@@ -8,10 +8,11 @@
  */
 
 var request = require('request'); // "Request" library
-
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
-
+const fs = require('fs');
+var client_id = '19a5e88685af46759ece15d390334bd5'; // Your client id
+var client_secret = '25e0127d9eb9466aa7d4e7f92176a535'; // Your secret
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+const alphabet = 'abcdefghijklmnopqrstuvwxyz'; //hi
 // your application requests authorization
 var authOptions = {
   url: 'https://accounts.spotify.com/api/token',
@@ -25,19 +26,35 @@ var authOptions = {
 };
 
 request.post(authOptions, function(error, response, body) {
-  if (!error && response.statusCode === 200) {
+	var results = [];
+	for(var letter in alphabet) {
+		for(var i=0; i<2; i++) {
+			// use the access token to access the Spotify Web API
+			var token = body.access_token;
+			var options = {
+			  url: 'https://api.spotify.com/v1/search?q=artist:' + letter + '*&type=artist&market=US&limit=50&offset=' + (10000 + i*50),
+			  headers: {
+				'Authorization': 'Bearer ' + token
+			  },
+			  json: true
+			};
+			request.get(options, function(error, response, body) {
+				console.log(JSON.stringify(body,null,2));
+				//console.log("\n\n");
+				results.push(body);
+				  
+				fs.appendFileSync("../pain4.txt",JSON.stringify(body,null,2), function(err) {
+					if(err) {
+						return console.log(err);
+					}
+					console.log("saved file");
+				});
 
-    // use the access token to access the Spotify Web API
-    var token = body.access_token;
-    var options = {
-      url: 'https://api.spotify.com/v1/users/jmperezperez',
-      headers: {
-        'Authorization': 'Bearer ' + token
-      },
-      json: true
-    };
-    request.get(options, function(error, response, body) {
-      console.log(body);
-    });
-  }
+			});
+			
+		}
+	}
+	//result = JSON.stringify(results,null,2);
+	//console.log(result);
+	
 });
