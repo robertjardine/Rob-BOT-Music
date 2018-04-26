03ffb9894dcdeace6064477f04d66556681b27fa
ert00034 <31933424+ert00034@users.noreply.github.com>
2018-04-26 02:13:17 -0500

    Added top_songer
    Turns our list of artist IDs into a list of top songs by each artist (well, the JSON returned by Spotify's API)
    
    Adjust the sleep to avoid timeouts until we can do it better

---
+++ jsonLinesToArtistIDs.py
@@ -0,0 +1,10 @@
+import json
+
+with open('formattedJSON.txt','r') as f, open('artistIDList.txt','w',errors='replace') as g: 
+    for x in f:
+        x = x.rstrip()
+        if not x: continue
+        jsonLoader = json.loads(x)
+        for thisElement in jsonLoader['artists']['items']:
+            g.write(thisElement['id'])
+            g.write('\n')
---
+++ top_songer/app.js
@@ -0,0 +1,69 @@
+/**
+ * There's no way this file could be written any better
+ */
+const fs = require('fs');
+var ids = [];
+var lineReader = require('readline').createInterface({
+  input: require('fs').createReadStream('../artistIDList.txt')
+});
+
+lineReader.on('line', function (line) {
+  ids.push(line);
+});
+
+var request = require('request'); // "Request" library
+
+var client_id = '19a5e88685af46759ece15d390334bd5'; // Your client id
+var client_secret = '25e0127d9eb9466aa7d4e7f92176a535'; // Your secret
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+// your application requests authorization
+var authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+};
+
+request.post(authOptions, function(error, response, body) {
+	var alphabet = 'abcdefghijklmnopqrstuvwxyz';
+	var results = [];
+	for(var j=0; j<2600; j++) {
+        sleep(1000);
+		var filename = "topSongs.txt";
+        var currentArtistID = ids[j];
+        // use the access token to access the Spotify Web API
+        var token = body.access_token;
+        var options = {
+          url: 'https://api.spotify.com/v1/artists/'+currentArtistID,
+          headers: {
+            'Authorization': 'Bearer ' + token
+          },
+          json: true
+        };
+        request.get(options, function(error, response, body) {
+            //console.log(JSON.stringify(body,null,2));
+            //console.log("\n\n");
+            results.push(body);
+            
+            fs.appendFileSync("../outputs/" + filename,JSON.stringify(body,null,2) + "^", function(err) {
+                if(err) {
+                    console.log("ERROR:");
+                    return console.log(err);
+                }
+                console.log("saved file");
+            });
+
+        });
+			
+	}
+	//result = JSON.stringify(results,null,2);
+	//console.log(result);
+	
+});
+function sleep(millis) {
+    return new Promise(resolve => setTimeout(resolve, millis));
+}
