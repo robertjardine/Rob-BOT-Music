99d6c4a88c60d827ae9455894cc5be1b2cad4e00
ert00034 <31933424+ert00034@users.noreply.github.com>
2018-04-26 01:40:13 -0500

    Finished python scripts
    client_credentials -> outputs/output.txt
    WaterToWine.py + output.txt -> formattedJSON.txt
    jsonLinesToArtistNames.py + formattedJSON.txt -> artistList.txt

--- jsonLinesToArtistNames.py
+++ jsonLinesToArtistNames.py
@@ -1,19 +1,10 @@
 import json
 
-with open('formattedJSON.txt','r') as f, open('artistList.txt','w') as g: 
-	for x in f:
-		x = x.rstrip()
-		if not x: continue
-		jsonLoader = json.loads(x)
-		for thisElement in jsonLoader['artists']['items']:
-			for eachThing in thisElement:
-				if "name" in eachThing:
-					print(eachThing)
-					print("\n")
-				#print(eachThing)
-				#print("\n")
-			#listThis = thisElement.split(",")
-			#for maybeName in thisList:
-			#	
-		#g.write(json.dumps(jsonLoader))
-		#g.write
+with open('formattedJSON.txt','r') as f, open('artistList.txt','w',errors='replace') as g: 
+    for x in f:
+        x = x.rstrip()
+        if not x: continue
+        jsonLoader = json.loads(x)
+        for thisElement in jsonLoader['artists']['items']:
+            g.write(thisElement['name'])
+            g.write('\n')
