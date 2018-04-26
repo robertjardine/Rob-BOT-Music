0b1a4b2f4845ffa102110fdc214a555ff5a421c5
ert00034 <31933424+ert00034@users.noreply.github.com>
2018-04-25 23:19:00 -0500

    Half way to making artist lists automatically

--- client_credentials/app.js
+++ client_credentials/app.js
@@ -12,7 +12,6 @@ const fs = require('fs');
 var client_id = '19a5e88685af46759ece15d390334bd5'; // Your client id
 var client_secret = '25e0127d9eb9466aa7d4e7f92176a535'; // Your secret
 var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
-const alphabet = 'abcdefghijklmnopqrstuvwxyz'; //hi
 // your application requests authorization
 var authOptions = {
   url: 'https://accounts.spotify.com/api/token',
@@ -26,25 +25,30 @@ var authOptions = {
 };
 
 request.post(authOptions, function(error, response, body) {
+	var alphabet = 'abcdefghijklmnopqrstuvwxyz';
 	var results = [];
 	for(var j=0; j<26; j++) {
+		var filename = "output.txt";
 		for(var i=0; i<2; i++) {
+			console.log("Letter " + alphabet.charAt(j) + ", part " + (i+1));
 			// use the access token to access the Spotify Web API
 			var token = body.access_token;
 			var options = {
-			  url: 'https://api.spotify.com/v1/search?q=artist:' + alphabet[j] + '*&type=artist&market=US&limit=50&offset=' + (10000 + i*50),
+			  url: 'https://api.spotify.com/v1/search?q=artist:' + alphabet.charAt(j) + '*&type=artist&market=US&limit=50&offset=' + (10000 + i*50),
 			  headers: {
 				'Authorization': 'Bearer ' + token
 			  },
 			  json: true
 			};
 			request.get(options, function(error, response, body) {
-				console.log(JSON.stringify(body,null,2));
+				//console.log(JSON.stringify(body,null,2));
 				//console.log("\n\n");
 				results.push(body);
-				  
-				fs.appendFileSync("../pain4.txt",JSON.stringify(body,null,2), function(err) {
+				
+				console.log("appending to " + filename);
+				fs.appendFileSync("../outputs/" + filename,JSON.stringify(body,null,2) + "^", function(err) {
 					if(err) {
+						console.log("ERROR:");
 						return console.log(err);
 					}
 					console.log("saved file");
