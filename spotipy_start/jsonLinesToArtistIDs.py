0b7f83a4d994432fada5da499ebdb243cedd6720
ert00034 <31933424+ert00034@users.noreply.github.com>
2018-04-30 19:21:39 -0500

    Finished migrating to spotipy
    It deals with rate limits automatically! It's beautiful!
    
    songInfoer won't actually be used in the end - instead just do it on the fly

---
+++ spotipy_start/jsonLinesToArtistIDs.py
@@ -0,0 +1,11 @@
+import json
+
+with open('output.txt','r') as f, open('artistIDList.txt','w',errors='replace') as g: 
+    for x in f:
+        x = x.rstrip()
+        if not x: continue
+        jsonLoader = json.loads(x)
+        if 'artists' in jsonLoader:
+            for thisElement in jsonLoader['artists']['items']:
+                g.write(thisElement['id'])
+                g.write('\n')
