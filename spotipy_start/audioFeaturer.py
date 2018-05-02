d42a4aac33b51537e1c3d2ceb9b71fd72b415ec7
ert00034 <31933424+ert00034@users.noreply.github.com>
2018-05-02 15:15:39 -0500

    audioFeaturer is amazing!
    it's like 500x as fast as audio analysis

--- spotipy_start/audioFeaturer.py
+++ spotipy_start/audioFeaturer.py
@@ -4,18 +4,22 @@ import spotipy.util as util
 import json
 from time import sleep
 from spotipy.oauth2 import SpotifyClientCredentials
-#NOTE: THIS TAKES WAY TOO LONG TO RUN ON LARGE DATA SETS - 2 SONGS PER SECOND
-#Consider using alternative call (audio_features instead of audio_analysis?) for quicker results
+
 client_credentials_manager = SpotifyClientCredentials(client_id="19a5e88685af46759ece15d390334bd5", client_secret="25e0127d9eb9466aa7d4e7f92176a535")
 
 spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager,requests_timeout=10)
 results = ''
-fileOut = open("audioInfo.txt","w")
-
+fileOut = open("audioFeatures.txt","w")
+idList = []
+# Audio features takes a list of song IDs and returns their features
+# Spotipy says it maxes at 50 IDs, but Spotify's API claims you can use 100 at a time
 with open("songIDList.txt","r",encoding="UTF-8") as fileIn:
 	for line in fileIn:
 		line = line.strip("\n")
 		print(line)
-		results = spotify.audio_analysis(line)
-		fileOut.write(json.dumps(results))
-		fileOut.write("\n")
+		idList.append(line)
+		if( len(idList) >= 50 ):
+			results = spotify.audio_features(idList)
+			fileOut.write(json.dumps(results))
+			fileOut.write("\n")
+			idList.clear()
--- spotipy_start/topSongerButWithTimeouts.py
+++ spotipy_start/topSongerButWithTimeouts.py
@@ -4,18 +4,17 @@ import spotipy.util as util
 import json
 from time import sleep
 from spotipy.oauth2 import SpotifyClientCredentials
-#NOTE: THIS TAKES WAY TOO LONG TO RUN ON LARGE DATA SETS - 2 SONGS PER SECOND
-#Consider using alternative call (audio_features instead of audio_analysis?) for quicker results
+
 client_credentials_manager = SpotifyClientCredentials(client_id="19a5e88685af46759ece15d390334bd5", client_secret="25e0127d9eb9466aa7d4e7f92176a535")
 
 spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager,requests_timeout=10)
 results = ''
-fileOut = open("audioInfo.txt","w")
+fileOut = open("topSongsJSONTimeout.txt","w")
 
-with open("songIDList.txt","r",encoding="UTF-8") as fileIn:
+with open("artistIDListTimeout.txt","r",encoding="UTF-8") as fileIn:
 	for line in fileIn:
 		line = line.strip("\n")
 		print(line)
-		results = spotify.audio_analysis(line)
+		results = spotify.artist_top_tracks(artist_id=line,country='US')
 		fileOut.write(json.dumps(results))
 		fileOut.write("\n")
