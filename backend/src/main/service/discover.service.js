84a406fa470b79d1a755324f7553889e7e06440a
Robert Jardine <25288229+yumewokanaeyou@users.noreply.github.com>
2018-10-06 18:12:42 -0500

    Pull discover logic to the service (#16)

--- backend/src/main/service/discover.service.js
+++ backend/src/main/service/discover.service.js
@@ -1,6 +1,7 @@
-const discoverDelegate = require('../delegate/discover.delegate');
+const spotifyDelegate = require('../delegate/spotify.delegate');
 
 const NUM_SONGS_IN_PLAYLIST = 15;
+const MAX_OFFSET = 9999;
 
 /**
  * Generate a random list of Track objects of length NUM_SONGS_IN_PLAYLIST
@@ -19,17 +20,16 @@ exports.getDiscoverPlaylist = function() {
 }
 
 /**
- * TODO: Take care of null top songs
  * Retrieve a random Track
  * @return { Track } Random top track from a random artist
  */
 function getRandomTrack() {
     return new Promise(async (resolve, reject) => {
         try {
-            let randomArtistId = await discoverDelegate.getRandomArtist();
+            let randomArtistId = await getRandomArtist();
             let randomArtistTopSongs = null;
             do {
-                randomArtistTopSongs = await discoverDelegate.getTopSongs(randomArtistId);
+                randomArtistTopSongs = await getTopSongs(randomArtistId);
             } while (!randomArtistTopSongs || randomArtistTopSongs.length == 0);
             let index = randomIndexGenerator(randomArtistTopSongs.length-1);
             resolve(randomArtistTopSongs[index]);
@@ -37,6 +37,35 @@ function getRandomTrack() {
             reject(error);
         }
     });          
+}
+
+/**
+ * Retrieve a random artist id from the Spotify API
+ * API has a limit of an offset of 10,000
+ * @return { number } id of a Spotify artist
+ */
+function getRandomArtist() {
+    return new Promise((resolve, reject) => {
+        let offset = Math.floor(Math.random() * (MAX_OFFSET + 1));
+        let url = `https://api.spotify.com/v1/search?q=year%3A0000-9999&type=artist&limit=1&offset=${offset}`;
+        spotifyDelegate.requestToSpotify(url)
+            .then(data => resolve(data.artists.items[0].id))
+            .catch(error => reject(error));
+    });
+}
+
+/**
+ * Uses an artist Id to retrieve the artist's top songs from the Spoitfy API
+ * @param { number } artistId Number that uniquely identifies an artists in the Spotify API
+ * @return { Track[] } Array of Track objects that contain the top songs for the artist Id
+ */
+function getTopSongs(artistId) {
+    return new Promise((resolve, reject) => {
+        let url = `https://api.spotify.com/v1/artists/${artistId}/top-tracks?country=US`;
+        spotifyDelegate.requestToSpotify(url)
+            .then(data => resolve(data.tracks))
+            .catch(error => reject(error));
+    });  
 }
 
 /**
